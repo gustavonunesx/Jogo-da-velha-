9743c9266cc0ffdfdf6d81d22e959d7070e5230c
= <=>
2025-08-25 22:46:11 -0300

    commits finais

--- jogo-da-velha/src/App.js
+++ jogo-da-velha/src/App.js
@@ -1,40 +1,40 @@
-import { useState } from 'react';
 import './App.css';
+import { useState } from 'react'; 
 
-function Square(){
-  const [valor,setValor] = useState(null);
-
-  function handleClick(){
-    setValor('X');
-  }
-
-  return(
-    <button className='square' onClick={handleClick}> {valor} </button>
+function Square({ valor, onSquareClick }) { 
+  return (
+    <button className="square" onClick={onSquareClick}> 
+      {valor}
+    </button>
   );
 }
 
-export default function Tabuleiro(){
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null)); 
+  
+  function handleClick(i) { 
+    const nextSquares = squares.slice(); 
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
 
-    return (
+  return (
+    <div> 
       <div>
-        <div>
-          <Square/>
-          <Square/>
-          <Square/>
-        </div>
-        
-        <div>
-          <Square/>
-          <Square/>
-          <Square/>
-        </div>
-        
-        <div>
-          <Square/>
-          <Square/>
-          <Square/>
-        </div>
-      
-     </div>
-    );
+        <Square valor={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square valor={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square valor={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div>
+        <Square valor={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square valor={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square valor={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div>
+        <Square valor={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square valor={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square valor={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </div>
+  );
 }
